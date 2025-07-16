c3072e30d0c03e9bbf5ac1f13c00569973a2f14b
sithika2000 <snimsilu2000k@gmail.com>
2025-07-16 13:24:52 +0530

    added main and accomo

---
+++ lib/accommodation.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+
+void main() {
+  runApp(TravelWishApp());
+}
+
+class TravelWishApp extends StatelessWidget {
+  const TravelWishApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AccommodationPage(),
+    );
+  }
+}
+
+class AccommodationPage extends StatefulWidget {
+  const AccommodationPage({super.key});
+
+  @override
+  _AccommodationPageState createState() => _AccommodationPageState();
+}
+
+class _AccommodationPageState extends State<AccommodationPage> {
+  DateTime startDate = DateTime.now();
+  DateTime endDate = DateTime.now().add(Duration(days: 2));
+
+  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: isStartDate ? startDate : endDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      setState(() {
+        if (isStartDate) {
+          startDate = picked;
+        } else {
+          endDate = picked;
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.travel_explore, color: Colors.white),
+                SizedBox(width: 8),
+                Text("TravelWish", style: TextStyle(color: Colors.white)),
+              ],
+            ),
+            Icon(Icons.notifications, color: Colors.white),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {},
+                ),
+                Text("Accommodation",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(width: 40),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              margin: EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => _selectDate(context, true),
+                    child: Row(
+                      children: [
+                        Icon(Icons.calendar_today, color: Colors.blue),
+                        SizedBox(width: 5),
+                        // Text("< ${DateFormat('E, d MMM').format(startDate)} >"),
+                      ],
+                    ),
+                  ),
+                  Text("|"),
+                  GestureDetector(
+                    onTap: () => _selectDate(context, false),
+                    child: Row(
+                      children: [
+                        Icon(Icons.calendar_today, color: Colors.blue),
+                        SizedBox(width: 5),
+                        // Text("< ${DateFormat('E, d MMM').format(endDate)} >"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.sort),
+                  label: Text("Sort By"),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.filter_list),
+                  label: Text("Filter By"),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Column(
+              children: List.generate(5, (index) => accommodationBox()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget accommodationBox() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              "https://via.placeholder.com/80",
+              width: 80,
+              height: 80,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Hotel Name",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Row(
+                  children: [
+                    Text("4.5"),
+                    SizedBox(width: 5),
+                    Row(
+                      children: List.generate(
+                          5,
+                          (index) =>
+                              Icon(Icons.star, color: Colors.yellow, size: 16)),
+                    ),
+                  ],
+                ),
+                Text("Fantastic", style: TextStyle(color: Colors.green)),
+                Container(
+                  margin: EdgeInsets.only(top: 8),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "\$120 per night",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, dynamic>> options = [
+    {'icon': Icons.directions_bus, 'label': 'Public Transport'},
+    {'icon': Icons.hotel, 'label': 'Accommodation'},
+    {'icon': Icons.restaurant, 'label': 'Restaurant'},
+    {'icon': Icons.local_hospital, 'label': 'Emergency'},
+    {'icon': Icons.explore, 'label': 'Things to do'},
+    {'icon': Icons.local_taxi, 'label': 'Taxi'},
+    {'icon': Icons.people, 'label': 'Guides'},
+    {'icon': Icons.map, 'label': 'Map'},
+    {'icon': Icons.miscellaneous_services, 'label': 'Services'},
+  ];
+
+  final List<Map<String, String>> popularPlaces = [
+    {'image': 'assets/image01.jpeg', 'name': 'Beach'},
+    {'image': 'assets/image02.jpg', 'name': 'Mountain'},
+    {'image': 'assets/image01.jpeg', 'name': 'City'},
+    {'image': 'assets/image02.jpg', 'name': 'Resort'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Image.asset('assets/logo.png', height: 40),
+                SizedBox(width: 8),
+                Text(
+                  'travelWish',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello ,',
+                      style: TextStyle(fontSize: 18, color: Colors.black54),
+                    ),
+                    Text(
+                      'Chandupa Sasmitha', //username
+                      style: TextStyle(fontSize: 24, color: Colors.black),
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage:
+                      AssetImage('assets/image02.jpeg'), //background image
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemCount: options.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 51,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        options[index]['icon'],
+                        size: 30,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(options[index]['label'], textAlign: TextAlign.center),
+                  ],
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Popular  ', // a title
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: popularPlaces.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 151,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage(popularPlaces[index]['image']!),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(5),
+                        color: Colors.black.withOpacity(0.5),
+                        child: Text(
+                          popularPlaces[index]['name']!,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Explore  ',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 102, 183, 251),
+        child: Icon(Icons.search),
+      ),
+    );
+  }
+}
