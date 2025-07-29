9fc3be315b6c5bcd49fffe884c981c9dd17d5c33
chandupasasmitha <chandupasasmitha@gmail.com>
2025-07-29 16:23:32 +0530

    main branch error fixing

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static const String baseUrl = "http://localhost:2000/api/";
+  static const String baseUrl = "http://10.0.2.2:2000/api/";
 
   // ==================== USER METHODS ====================
   static adduser(Map udata) async {
